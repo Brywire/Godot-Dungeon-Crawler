c94b2bdfb5353ac4b3031f083a642d05b3b2c266
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-06-04 10:56:33 +0200

    [+] FiniteStateMachine

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -1,17 +1,20 @@
 extends KinematicBody2D
 class_name Character, "res://Art/v1.1 dungeon crawler 16X16 pixel pack/heroes/knight/knight_idle_anim_f0.png"
 
+const FRICTION: float = 0.15
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(int) var acceleration: int = 40
+export(int) var max_speed: int = 100
 
+var mov_direction: Vector2 = Vector2.ZERO
+var velocity: Vector2 = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(_delta: float) -> void:
+	velocity = move_and_slide(velocity)
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+	
+	
+func move() -> void:
+	mov_direction = mov_direction.normalized()
+	velocity += mov_direction * acceleration
+	velocity = velocity.clamped(max_speed)
