9e115c2066e61cf4cc3619c06755e56e01a5c0d2
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-06-05 12:32:48 +0200

    [+] Flying creature hurt & dead state

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -3,9 +3,12 @@ class_name Character, "res://Art/v1.1 dungeon crawler 16X16 pixel pack/heroes/kn
 
 const FRICTION: float = 0.15
 
+export(int) var hp: int = 2
+
 export(int) var acceleration: int = 40
 export(int) var max_speed: int = 100
 
+onready var state_machine: Node = get_node("FiniteStateMachine")
 onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
 
 var mov_direction: Vector2 = Vector2.ZERO
@@ -20,3 +23,14 @@ func move() -> void:
 	mov_direction = mov_direction.normalized()
 	velocity += mov_direction * acceleration
 	velocity = velocity.clamped(max_speed)
+
+
+
+func take_damage(dam: int, dir: Vector2, force: int) -> void:
+	hp -= dam
+	if hp > 0:
+		state_machine.set_state(state_machine.states.hurt)
+		velocity += dir * force
+	else:
+		state_machine.set_state(state_machine.states.dead)
+		velocity += dir * force * 2
